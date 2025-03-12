98376478071a3eca3fb85b92948372ba8c82a918
Abhishek-DVD <abhishekdvd404@gmail.com>
2025-03-12 10:59:42 -0700

    bug-fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const MICRO_API_URL = "https://api.microlink.io";
-export const LEETCODE_URL = "https://leetcode-data-1.onrender.com/";
+export const LEETCODE_URL = "https://leetcode-data-1.onrender.com";
