ac8491aa6a0ee685ac07db73c00dcde4f614cb54
Abhishek-DVD <abhishekdvd404@gmail.com>
2026-05-22 16:10:40 +0530

    fixing vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const base = process.env.VERCEL ? '/' : '/personal-portfolio/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-portfolio/',
+  base,
   plugins: [react()],
 })
